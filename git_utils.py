2c98f48ab0b0ea501cb7527a92813f437994f011
Dave Bort <dbort@google.com>
2018-07-30 12:03:15 -0700

    [git-file-format] Add --lines flag to only format modified lines
    Change-Id: Iab6b8316eab6a2d7c9ce7253dcda53f8fd7ee1aa

--- git_utils.py
+++ git_utils.py
@@ -2,8 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import collections
 import os
 import os.path
+import re
 import subprocess
 
 
@@ -55,6 +57,69 @@ def get_diff_files():
     git_root_path = get_git_root()
     paths = filter(len, subprocess.check_output(list_command).split("\n"))
     return [ os.path.join(git_root_path, x) for x in paths ]
+
+
+def get_modified_lines(paths):
+    """Returns ranges of lines modified in the specified files.
+
+    Args:
+        paths: A sequence of filenames to check for modified lines. Typically
+               a subset of the paths returned by get_diff_files().
+
+    Returns:
+        A dict mapping elements of `paths` to sequences of 1-indexed
+        (start-line-number, num-modified-lines) tuples.
+    """
+    # For a line like |+++ b/path/to/file.py|, extracts the filename
+    # "path/to/file.py" in group 1, intentionally skipping the 'b/' component.
+    filename_line_re = re.compile(r'^\+\+\+ [^/]+/(.*)')
+
+    # For lines like
+    #     @@ -9,1 +10,2 @@ Trailing text
+    # or
+    #     @@ -11,0 +12 @@ Trailing text
+    # extracts the second field ('10,2' or '12' in the examples) in group 1.
+    line_number_re = re.compile(r'^@@ -[0-9,]+ \+([0-9,]+) @@')
+
+    git_root_path = get_git_root()
+
+    # Diff output may be large, so stream the output rather than using
+    # communicate().
+    diff_command = ["git", "diff-index", "-U0", _get_diff_base()] + paths
+    p = subprocess.Popen(diff_command, stdout=subprocess.PIPE)
+
+    # Set up return dict. All input paths should have entries even if there
+    # are no diffs.
+    paths_to_ranges = collections.OrderedDict()
+    for path in paths:
+        paths_to_ranges[path] = []
+
+    ranges = None  # Pointer to the current file's entry in `paths_to_ranges`
+    with p.stdout:  # 'with' will close stdout on any exception
+        for line in p.stdout:
+            m = filename_line_re.match(line)
+            if m:
+                # Use absolute paths as keys.
+                fname = os.path.join(git_root_path, m.group(1))
+                ranges = paths_to_ranges[fname]
+                continue
+            m = line_number_re.match(line)
+            if m:
+                fields = m.group(1).split(',')
+                if len(fields) == 1:
+                    # If there's only one number,
+                    # the range contains a single line.
+                    fields.append('1')
+                start = int(fields[0])
+                count = int(fields[1])
+                if count > 0:  # If zero, the range was deleted.
+                    ranges.append((start, count))
+    if p.wait():
+        # Non-zero exit status.
+        raise subprocess.CalledProcessError(p.returncode, diff_command)
+
+    return paths_to_ranges
+
 
 def get_all_files():
     """Returns absolute paths to all files in the git repo under the current
